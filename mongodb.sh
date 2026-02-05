def32af5df4743de509a88e5a5ff70aac2a18f84
DAGGUPATI-SRIKANTH <daggupatisrikanth35@gmail.com>
2026-02-05 17:58:01 -0600

    actions

--- mongodb.sh
+++ mongodb.sh
@@ -16,10 +16,10 @@ mkdir -p $LOGS_FOLDER
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo -e $2 ...$R FAILURE $N" | tee -a $LOGS_FILE
+        echo -e "$2 ...$R FAILURE $N" | tee -a $LOGS_FILE
         exit 1
     else
-        echo -e $2 ...$G SUCCESS $N" | tee -a $LOGS_FILE
+        echo -e "$2 ...$G SUCCESS $N" | tee -a $LOGS_FILE
     fi
 }
  
