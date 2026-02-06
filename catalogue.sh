6e5a144b45cf3104aeafaabfa1a774124bf42cee
DAGGUPATI-SRIKANTH <daggupatisrikanth35@gmail.com>
2026-02-05 18:53:06 -0600

    actions

--- catalogue.sh
+++ catalogue.sh
@@ -7,6 +7,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.srikanthdaggupati.com
 
 if [ $USERID -ne 0 ]; then
     echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
@@ -65,3 +66,8 @@ systemctl daemon-reload
 systemctl enable catalogue &>>$LOGS_FILE
 systemctl start catalogue
 VALIDATE $? "Starting and enabling Catalogue"
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+dnf install mongodb-mongosh -y 
+
+mongosh --host $MONGODB_HOST </app/db/master-data.js
