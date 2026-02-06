f99c5e86217e843e00fefd5c3cb2613674b1d2ae
DAGGUPATI-SRIKANTH <daggupatisrikanth35@gmail.com>
2026-02-05 20:25:47 -0600

    actions

--- redis.sh
+++ redis.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-roboshop"
 LOGS_FILE="$LOGS_FOLDER/$0.log"
@@ -16,22 +17,23 @@ mkdir -p $LOGS_FOLDER
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo -e "$2 ...$R FAILURE $N" | tee -a $LOGS_FILE
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
         exit 1
     else
-        echo -e "$2 ...$G SUCCESS $N" | tee -a $LOGS_FILE
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
     fi
 }
 
-dnf module disable redis -y $LOGS_FILE
-dnf module enable redis:7 -y $LOGS_FILE
-VALIDATE $? "Enabling Redis:7"
+dnf module disable redis -y &>>$LOGS_FILE
+dnf module enable redis:7 -y &>>$LOGS_FILE
+VALIDATE $? "Enable Redis:7"
 
-dnf install redis -y $LOGS_FILE
-VALIDATE $? "Installing Redis"
+dnf install redis -y  &>>$LOGS_FILE
+VALIDATE $? "Installed Redis"
 
 sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
 VALIDATE $? "Allowing remote connections"
-systemctl enable redis $LOGS_FILE
+
+systemctl enable redis &>>$LOGS_FILE
 systemctl start redis 
-VALIDATE $? "Starting Redis"
+VALIDATE $? "Enabled and started Redis"
