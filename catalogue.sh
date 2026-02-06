25b02360c2afaf2a21ce49c43e766f295e3ca0c5
DAGGUPATI-SRIKANTH <daggupatisrikanth35@gmail.com>
2026-02-05 19:25:37 -0600

    actions

--- catalogue.sh
+++ catalogue.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-roboshop"
 LOGS_FILE="$LOGS_FOLDER/$0.log"
@@ -18,10 +19,10 @@ mkdir -p $LOGS_FOLDER
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo -e "$2 ...$R FAILURE $N" | tee -a $LOGS_FILE
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
         exit 1
     else
-        echo -e "$2 ...$G SUCCESS $N" | tee -a $LOGS_FILE
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
     fi
 }
 
@@ -29,55 +30,56 @@ dnf module disable nodejs -y &>>$LOGS_FILE
 VALIDATE $? "Disabling NodeJS Default version"
 
 dnf module enable nodejs:20 -y &>>$LOGS_FILE
-VALIDATE $? "Enabling NodeJS 20 version"
+VALIDATE $? "Enabling NodeJS 20"
 
 dnf install nodejs -y &>>$LOGS_FILE
-VALIDATE $? "Installing NodeJS"
+VALIDATE $? "Install NodeJS"
 
 id roboshop &>>$LOGS_FILE
 if [ $? -ne 0 ]; then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
-    VALIDATE $? "Creating System User"
-else 
-    echo -e "Roboshop user already exits ...$Y SKIPPING $N"
+    VALIDATE $? "Creating system user"
+else
+    echo -e "Roboshop user already exist ... $Y SKIPPING $N"
 fi
-mkdir -p /app 
-VALIDATE $? "Creating App Directory"
 
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOGS_FILE
-VALIDATE $? "Downloading Catalogue code"
+mkdir -p /app 
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip  &>>$LOGS_FILE
+VALIDATE $? "Downloading catalogue code"
 
 cd /app
-VALIDATE $? "Changing to app directory"
+VALIDATE $? "Moving to app directory"
 
 rm -rf /app/*
 VALIDATE $? "Removing existing code"
 
 unzip /tmp/catalogue.zip &>>$LOGS_FILE
-VALIDATE $? "Extracting Catalogue code"
+VALIDATE $? "Uzip catalogue code"
 
-npm install &>>$LOGS_FILE
-VALIDATE $? "Installing NodeJS dependencies"
+npm install  &>>$LOGS_FILE
+VALIDATE $? "Installing dependencies"
 
 cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
 VALIDATE $? "Created systemctl service"
 
 systemctl daemon-reload
-systemctl enable catalogue &>>$LOGS_FILE
+systemctl enable catalogue  &>>$LOGS_FILE
 systemctl start catalogue
-VALIDATE $? "Starting and enabling Catalogue"
+VALIDATE $? "Starting and enabling catalogue"
 
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
-dnf install mongodb-mongosh -y 
+dnf install mongodb-mongosh -y &>>$LOGS_FILE
 
-INDEX=$(mongosh --host $MONGODB_HOST --quiet --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+INDEX=$(mongosh --host $MONGODB_HOST --quiet  --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 
 if [ $INDEX -le 0 ]; then
     mongosh --host $MONGODB_HOST </app/db/master-data.js
     VALIDATE $? "Loading products"
 else
-    echo -e "Products already loaded ...$Y SKIPPING $N"
+    echo -e "Products already loaded ... $Y SKIPPING $N"
 fi
 
 systemctl restart catalogue
-VALIDATE $? "Restarting Catalogue"
+VALIDATE $? "Restarting catalogue"
